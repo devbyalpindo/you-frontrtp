3361b904a0f70d1241d833ae4e0baab462472eb2
alpindo <alpindo888@gmail.com>
2024-02-11 12:00:24 +0700

    update facebook pixel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,9 +5,25 @@ import Header from "../components/layouts/header/header";
 import Footer from "../components/layouts/Footer/footer";
 import { store } from "../components/redux/store";
 import { Provider } from "react-redux";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [loading, setLoading] = useState<boolean>(true);
+
+  const router = useRouter()
+
+  useEffect(() => {
+    import('react-facebook-pixel')
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init('566855558929284') // facebookPixelId
+        ReactPixel.pageView()
+
+        router.events.on('routeChangeComplete', () => {
+          ReactPixel.pageView()
+        })
+      })
+  }, [router.events])
 
   useEffect(() => {
     setTimeout(function () {
