081f167ef4d286d619fe3271fce3b5ecb2171319
alpindo <66691799+devbyalpindo@users.noreply.github.com>
2024-04-01 19:27:34 +0700

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,7 @@ const Home: React.FC<ProductProps> = ({ data }) => {
   const [valueSort, setValueSort] = useState("nothing");
   const dispatch = useDispatch();
   const selector = useSelector((state: any) => state.product);
-  const [baseUrl, setBaseUrl] = useState("https://www.youslot88s.co/");
+  const [baseUrl, setBaseUrl] = useState("https://uslot88.top/");
   const [referal, setReferal] = useState("VIPRTP88");
   const [dataProduct, setDataProduct] = useState<ProductTypes[]>(
     data.data[0].Products
